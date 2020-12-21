5581834fad50ce80238864221b3241a810b7a982
rakshit <rdsonnn@gmail.com>
2020-12-22 00:14:39 +0530

    reverse cipher

---
+++ reverse.py
@@ -0,0 +1,22 @@
+message = 'prajwal'
+translated = ''
+
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    i = i-1
+
+print(translated)
+
+
+message = '.srehto fo ypoc a eb ton od dna uoy eB'
+translated = ''
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    i = i-1
+
+
+print(translated)
